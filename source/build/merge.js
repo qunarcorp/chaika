be63bc216577f6d04d33de020bd132fd5f0a2f3c
汲国兴 <connorforg@gmail.com>
2019-03-07 16:05:53 +0800

    feat: 增加路由 order

--- source/build/merge.js
+++ source/build/merge.js
@@ -1,271 +1,297 @@
-let fs = require('fs-extra');
-let path = require('path');
-const { fail, success } = require('../utils').outLog;
-const { DEST_DIR } = require('../chaika_config');
-let glob = require('glob');
+let fs = require("fs-extra");
+let path = require("path");
+const { fail, success } = require("../utils").outLog;
+const { DEST_DIR } = require("../chaika_config");
+let glob = require("glob");
 let cwd = process.cwd();
-const crypto = require('crypto');
+const crypto = require("crypto");
 let cache = {};
 let needUpdate = (id, code, fn) => {
-  let sha1 = crypto
-    .createHash('sha1')
-    .update(code)
-    .digest('hex');
-  if (!cache[id] || cache[id] != sha1) {
-    cache[id] = sha1;
-    fn();
-  }
+    let sha1 = crypto
+        .createHash("sha1")
+        .update(code)
+        .digest("hex");
+    if (!cache[id] || cache[id] != sha1) {
+        cache[id] = sha1;
+        fn();
+    }
 };
 
 //业务线配置文件名
 let isMain;
 
 let spaceLine = (key, text) => {
-  return `//===================== ${key} ${text} =====================`;
+    return `//===================== ${key} ${text} =====================`;
 };
 
 //获取业务中各配置
 const getConfigFromProject = () => {
-  let nameSpaceRoutes = {};
-  let nameSpaceAlias = {};
-  let nameSpaceProjectPkg = {};
+    let nameSpaceRoutes = {};
+    let nameSpaceAlias = {};
+    let nameSpaceProjectPkg = {};
 
-  let moduleNamePattern = path.join(cwd, '.chaika_cache', 'chaika', 'qunar_*');
-  let moduleDirs = glob.sync(moduleNamePattern);
+    let moduleNamePattern = path.join(
+        cwd,
+        ".chaika_cache",
+        "chaika",
+        "qunar_*"
+    );
+    let moduleDirs = glob.sync(moduleNamePattern);
 
-  if (!isMain) {
-    moduleDirs.push(path.join(cwd, '.chaika_cache', 'chaika'));
-  }
-
-  fs.existsSync(path.join(cwd, 'source'))
-    ? moduleDirs.push(path.join(cwd, 'source'))
-    : moduleDirs.push(path.join(cwd));
-
-  moduleDirs.forEach(moduleDir => {
-    let pkg = {};
-    let pkgJsonPath = '';
-    let moduleName = '';
-    let appJsonData = {};
-
-    if (path.basename(moduleDir) === 'source') {
-      pkgJsonPath = path.join(cwd, 'package.json');
-    } else {
-      pkgJsonPath = path.join(moduleDir, 'package.json');
+    if (!isMain) {
+        moduleDirs.push(path.join(cwd, ".chaika_cache", "chaika"));
     }
 
-    try {
-      pkg = require(pkgJsonPath);
-    } catch (err) {
-      //项目无package.json依赖
-    }
+    fs.existsSync(path.join(cwd, "source"))
+        ? moduleDirs.push(path.join(cwd, "source"))
+        : moduleDirs.push(path.join(cwd));
 
-    appJsonData = require(path.join(moduleDir, 'app.json'));
+    moduleDirs.forEach(moduleDir => {
+        let pkg = {};
+        let pkgJsonPath = "";
+        let moduleName = "";
+        let appJsonData = {};
 
-    let routes = appJsonData['pages'].map(pagePath => {
-      let reg = /^\.\/pages\//; //是否以 ./pages开头
-      pagePath = reg.test(pagePath) ? pagePath : './' + pagePath;
-      return pagePath;
+        if (path.basename(moduleDir) === "source") {
+            pkgJsonPath = path.join(cwd, "package.json");
+        } else {
+            pkgJsonPath = path.join(moduleDir, "package.json");
+        }
+
+        try {
+            pkg = require(pkgJsonPath);
+        } catch (err) {
+            //项目无package.json依赖
+        }
+
+        appJsonData = require(path.join(moduleDir, "app.json"));
+        let order = appJsonData.order || 0;
+
+        let routes = appJsonData["pages"].map(pagePath => {
+            let reg = /^\.\/pages\//; //是否以 ./pages开头
+            pagePath = reg.test(pagePath) ? pagePath : "./" + pagePath;
+            return pagePath;
+        });
+
+        moduleName = pkg["module"] || pkg["name"];
+        nameSpaceRoutes[moduleName] = {
+            list: Array.from(new Set(routes)),
+            order: order
+        };
+        nameSpaceProjectPkg[moduleName] = pkg.dependencies;
+
+        nameSpaceAlias[moduleName] = appJsonData.alias;
     });
 
-    moduleName = pkg['module'] || pkg['name'];
-    nameSpaceRoutes[moduleName] = Array.from(new Set(routes));
-    nameSpaceProjectPkg[moduleName] = pkg.dependencies;
-
-    nameSpaceAlias[moduleName] = appJsonData.alias;
-  });
-
-  return {
-    nameSpaceRoutes,
-    nameSpaceAlias,
-    nameSpaceProjectPkg
-  };
+    return {
+        nameSpaceRoutes,
+        nameSpaceAlias,
+        nameSpaceProjectPkg
+    };
 };
 
 //业务线page route配置插入到app.js
 const injectPageRoute = nameSpaceRoutes => {
-  //获取业务page路由 和 alias配置
-  let nameSpace = nameSpaceRoutes;
+    //获取业务page路由 和 alias配置
+    let nameSpace = nameSpaceRoutes;
 
-  //获取app.js import依赖
-  let appJsPath = !isMain
-    ? path.join(cwd, '.chaika_cache', 'chaika', 'app.js')
-    : path.join(cwd, 'app.js');
-  let code = fs.readFileSync(appJsPath).toString();
-  //let {code} = getAppJsImportRoutes(path.join(cwd, '.chaika_cache', 'chaika' , 'app.js'));
+    //获取app.js import依赖
+    let appJsPath = !isMain
+        ? path.join(cwd, ".chaika_cache", "chaika", "app.js")
+        : path.join(cwd, "app.js");
+    let code = fs.readFileSync(appJsPath).toString();
+    //let {code} = getAppJsImportRoutes(path.join(cwd, '.chaika_cache', 'chaika' , 'app.js'));
 
-  //写入app.js
-  let allPageRoutes = Object.keys(nameSpace).map(key => {
-    return [
-      spaceLine(key, 'start'),
-      ...nameSpace[key],
-      spaceLine(key, 'end'),
-      '\n'
-    ];
-  });
-  //扁平化数组
-  allPageRoutes = [].concat(...allPageRoutes);
-  allPageRoutes = allPageRoutes.map(importValue => {
-    return /\.\/pages\//.test(importValue)
-      ? `import '${importValue}';`
-      : importValue;
-  });
-  code = allPageRoutes.join('\n') + '\n' + code;
-  let appJsDist = path.join(cwd, DEST_DIR, 'app.js');
-
-  needUpdate(appJsDist, code, () => {
-    fs.ensureFileSync(appJsDist);
-    fs.writeFile(appJsDist, code, err => {
-      if (err) {
-        fail('Merge route fail!' + err + '\n' + err.list);
-        return;
-      }
-      success(
-        'Merge page routes into ' + path.relative(cwd, appJsDist) + ' Success!'
-      );
+    //写入app.js
+    let allPageRoutes = Object.keys(nameSpace).map(key => {
+        return {
+            list: [
+                spaceLine(key, "start"),
+                ...nameSpace[key]["list"],
+                spaceLine(key, "end"),
+                "\n"
+            ],
+            order: nameSpace[key]["order"]
+        };
     });
-  });
+
+    allPageRoutes = allPageRoutes.sort((a, b) => {
+        return b.order - a.order;
+    });
+
+    allPageRoutes = allPageRoutes.map(item => {
+        return item.list;
+    });
+
+    //扁平化数组, 二维变一维
+    allPageRoutes = [].concat(...allPageRoutes);
+
+    allPageRoutes = allPageRoutes.map(importValue => {
+        return /\.\/pages\//.test(importValue)
+            ? `import '${importValue}';`
+            : importValue;
+    });
+    code = allPageRoutes.join("\n") + "\n" + code;
+    let appJsDist = path.join(cwd, DEST_DIR, "app.js");
+
+    needUpdate(appJsDist, code, () => {
+        fs.ensureFileSync(appJsDist);
+        fs.writeFile(appJsDist, code, err => {
+            if (err) {
+                fail("Merge route fail!" + err + "\n" + err.list);
+                return;
+            }
+            success(
+                "Merge page routes into " +
+                    path.relative(cwd, appJsDist) +
+                    " Success!"
+            );
+        });
+    });
 };
 
 //校验冲突: alias和npm
 const checkAliasConflict = (aliasSpace, type) => {
-  let config = {
-    alias: 'alias',
-    npm: 'node_modules'
-  };
+    let config = {
+        alias: "alias",
+        npm: "node_modules"
+    };
 
-  let aliasDepTree = {};
+    let aliasDepTree = {};
 
-  //aliasDepTree
-  /**
-   * {
-   *   @hotelCommon': [ 'train:source/common', 'hotel:source/common'],
-   *   ...
-   * }
-   *
-   */
-  Object.keys(aliasSpace).forEach(spaceName => {
-    let alias = aliasSpace[spaceName];
-    Object.keys(alias).forEach(aliasName => {
-      aliasDepTree[aliasName] = aliasDepTree[aliasName] || [];
-      let aliasValue = alias[aliasName];
-      aliasDepTree[aliasName].push(`${spaceName}:${aliasValue}`);
+    //aliasDepTree
+    /**
+     * {
+     *   @hotelCommon': [ 'train:source/common', 'hotel:source/common'],
+     *   ...
+     * }
+     *
+     */
+    Object.keys(aliasSpace).forEach(spaceName => {
+        let alias = aliasSpace[spaceName];
+        Object.keys(alias).forEach(aliasName => {
+            aliasDepTree[aliasName] = aliasDepTree[aliasName] || [];
+            let aliasValue = alias[aliasName];
+            aliasDepTree[aliasName].push(`${spaceName}:${aliasValue}`);
+        });
     });
-  });
 
-  let errMsg = '';
-  Object.keys(aliasDepTree).forEach(aliasName => {
-    //如果aliasDepTree中某一个别名被<=1的业务线配置, 肯定不会冲突.
-    if (aliasDepTree[aliasName].length <= 1) return;
+    let errMsg = "";
+    Object.keys(aliasDepTree).forEach(aliasName => {
+        //如果aliasDepTree中某一个别名被<=1的业务线配置, 肯定不会冲突.
+        if (aliasDepTree[aliasName].length <= 1) return;
 
-    //[ 'train:source/common', 'hotel:source/common']
-    //这种结构要经过处理，其实他们配置是一样的，不冲突, 经过处理后去重，如果 > 1，则肯定有冲突.
-    let alias = aliasDepTree[aliasName].map(value => {
-      return value.replace(/^\w+:/, '');
+        //[ 'train:source/common', 'hotel:source/common']
+        //这种结构要经过处理，其实他们配置是一样的，不冲突, 经过处理后去重，如果 > 1，则肯定有冲突.
+        let alias = aliasDepTree[aliasName].map(value => {
+            return value.replace(/^\w+:/, "");
+        });
+        alias = Array.from(new Set(alias));
+
+        if (alias.length > 1) {
+            errMsg += `\nConflict ${config[type]} name: ${aliasName}`;
+            aliasDepTree[aliasName].forEach(info => {
+                let infoList = info.split(":");
+                errMsg += `\nProject: ${infoList[0]}\n`;
+                errMsg += `    ${aliasName} >>> ${infoList[1]}`;
+            });
+            errMsg += "\n=========================================";
+        }
     });
-    alias = Array.from(new Set(alias));
 
-    if (alias.length > 1) {
-      errMsg += `\nConflict ${config[type]} name: ${aliasName}`;
-      aliasDepTree[aliasName].forEach(info => {
-        let infoList = info.split(':');
-        errMsg += `\nProject: ${infoList[0]}\n`;
-        errMsg += `    ${aliasName} >>> ${infoList[1]}`;
-      });
-      errMsg += '\n=========================================';
+    if (!errMsg) return;
+    let preText = "";
+    if (type === "alias") {
+        preText = "alias conflict! Merge abort. Must be fixed!";
+    } else {
+        preText = "package.json dependencies conflict! Must be fixed!";
     }
-  });
 
-  if (!errMsg) return;
-  let preText = '';
-  if (type === 'alias') {
-    preText = 'alias conflict! Merge abort. Must be fixed!';
-  } else {
-    preText = 'package.json dependencies conflict! Must be fixed!';
-  }
-
-  fail(preText + '\n' + errMsg);
+    fail(preText + "\n" + errMsg);
 };
 
 let merge = nameSpaceMap => {
-  let result = {};
-  Object.keys(nameSpaceMap).forEach(nameSpace => {
-    Object.assign(result, nameSpaceMap[nameSpace]);
-  });
-  return result;
+    let result = {};
+    Object.keys(nameSpaceMap).forEach(nameSpace => {
+        Object.assign(result, nameSpaceMap[nameSpace]);
+    });
+    return result;
 };
 
 //合并package.json
 let mergePkg = (nameSpaceAlias, nameSpaceProjectPkg) => {
-  let mainPkgPath = isMain
-    ? path.join(cwd, 'package.json')
-    : path.join(cwd, '.chaika_cache', 'chaika', 'package.json');
+    let mainPkgPath = isMain
+        ? path.join(cwd, "package.json")
+        : path.join(cwd, ".chaika_cache", "chaika", "package.json");
 
-  let mainPkg = require(mainPkgPath);
+    let mainPkg = require(mainPkgPath);
 
-  //见下面'重要'注释
-  mainPkg = JSON.stringify(mainPkg).replace(
-    new RegExp(`\\b${DEST_DIR}\\b`, 'g'),
-    'source'
-  );
-  mainPkg = JSON.parse(mainPkg);
+    //见下面'重要'注释
+    mainPkg = JSON.stringify(mainPkg).replace(
+        new RegExp(`\\b${DEST_DIR}\\b`, "g"),
+        "source"
+    );
+    mainPkg = JSON.parse(mainPkg);
 
-  let nameSpace = nameSpaceAlias;
-  mainPkg.nanachi = mainPkg.nanachi ? mainPkg.nanachi : {};
+    let nameSpace = nameSpaceAlias;
+    mainPkg.nanachi = mainPkg.nanachi ? mainPkg.nanachi : {};
 
-  let nanachi = mainPkg.nanachi;
-  let rootDependencies = mainPkg.dependencies || {};
-  let rootAlias = nanachi.alias ? nanachi.alias : {};
-  let moduleName = mainPkg.module || mainPkg.name;
+    let nanachi = mainPkg.nanachi;
+    let rootDependencies = mainPkg.dependencies || {};
+    let rootAlias = nanachi.alias ? nanachi.alias : {};
+    let moduleName = mainPkg.module || mainPkg.name;
 
-  let aliasSpaceMap = Object.assign({ [moduleName]: rootAlias }, nameSpace);
-  let dependenciesSpaceMap = Object.assign(
-    { [moduleName]: rootDependencies },
-    nameSpaceProjectPkg
-  );
+    let aliasSpaceMap = Object.assign({ [moduleName]: rootAlias }, nameSpace);
+    let dependenciesSpaceMap = Object.assign(
+        { [moduleName]: rootDependencies },
+        nameSpaceProjectPkg
+    );
 
-  //** dep 冲突测试;
-  //dependenciesSpaceMap['nnc_qunar_platform']['cookie2'] = '^0.3.6';
-  //dependenciesSpaceMap[moduleName]['cookie2'] = 'xxx';
+    //** dep 冲突测试;
+    //dependenciesSpaceMap['nnc_qunar_platform']['cookie2'] = '^0.3.6';
+    //dependenciesSpaceMap[moduleName]['cookie2'] = 'xxx';
 
-  //** alias 冲突测试;
-  //aliasSpaceMap['nnc_qunar_platform']['@common'] = '/source/x1';
-  //aliasSpaceMap[moduleName]['@common'] = '/source/x2';
+    //** alias 冲突测试;
+    //aliasSpaceMap['nnc_qunar_platform']['@common'] = '/source/x1';
+    //aliasSpaceMap[moduleName]['@common'] = '/source/x2';
 
-  //校验alias冲突
-  checkAliasConflict(aliasSpaceMap, 'alias');
+    //校验alias冲突
+    checkAliasConflict(aliasSpaceMap, "alias");
 
-  //校验package.json中dependencies冲突（某种意义上说, dependencies也是属于alias别名配置）
-  checkAliasConflict(dependenciesSpaceMap, 'npm');
+    //校验package.json中dependencies冲突（某种意义上说, dependencies也是属于alias别名配置）
+    checkAliasConflict(dependenciesSpaceMap, "npm");
 
-  mainPkg['nanachi']['alias'] = merge(aliasSpaceMap);
-  mainPkg['dependencies'] = merge(dependenciesSpaceMap);
+    mainPkg["nanachi"]["alias"] = merge(aliasSpaceMap);
+    mainPkg["dependencies"] = merge(dependenciesSpaceMap);
 
-  //重要: micro输出目录是nanachi输入目录, 业务线中各配置都是source为根路径, 需要更新为micro输出目录.
-  mainPkg['nanachi']['sourceDir'] = DEST_DIR.split('/')[1];
-  let mainJsonStr = JSON.stringify(mainPkg, null, 4).replace(
-    /\bsource\b/g,
-    DEST_DIR.split('/')[1]
-  );
+    //重要: micro输出目录是nanachi输入目录, 业务线中各配置都是source为根路径, 需要更新为micro输出目录.
+    mainPkg["nanachi"]["sourceDir"] = DEST_DIR.split("/")[1];
+    let mainJsonStr = JSON.stringify(mainPkg, null, 4).replace(
+        /\bsource\b/g,
+        DEST_DIR.split("/")[1]
+    );
 
-  let mainJsonDist = path.join(DEST_DIR.split('/')[0], 'package.json');
-  fs.writeFile(mainJsonDist, mainJsonStr, err => {
-    if (err) {
-      fail('Merge package.json fail!' + err + '\n' + err.list);
-      return;
-    }
-    success('Merge ' + path.relative(cwd, mainJsonDist) + ' Success!');
-  });
+    let mainJsonDist = path.join(
+        DEST_DIR.split("/")[0],
+        "package.json"
+    );
+    fs.writeFile(mainJsonDist, mainJsonStr, err => {
+        if (err) {
+            fail("Merge package.json fail!" + err + "\n" + err.list);
+            return;
+        }
+        success("Merge " + path.relative(cwd, mainJsonDist) + " Success!");
+    });
 };
 
 module.exports = (context, isMainProject) => {
-  isMain = isMainProject;
+    isMain = isMainProject;
+    let {
+        nameSpaceRoutes,
+        nameSpaceAlias,
+        nameSpaceProjectPkg
+    } = getConfigFromProject();
 
-  let {
-    nameSpaceRoutes,
-    nameSpaceAlias,
-    nameSpaceProjectPkg
-  } = getConfigFromProject();
-
-  injectPageRoute(nameSpaceRoutes, context);
-  mergePkg(nameSpaceAlias, nameSpaceProjectPkg, context);
+    injectPageRoute(nameSpaceRoutes, context);
+    mergePkg(nameSpaceAlias, nameSpaceProjectPkg, context);
 };
